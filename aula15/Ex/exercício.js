04bb46d0dd0fe25852cecf8964dd5c37bc1dcf6c
Israel Moreira <86491370+Railom96@users.noreply.github.com>
2023-10-03 09:30:15 -0300

    aula 15
    Ex aula 15

--- aula15/Ex/exercício.js
+++ aula15/Ex/exercício.js
@@ -1,6 +1,12 @@
-const numero = prompt ('Digite um número:');
+const numero = Number(prompt('Digite um número:'));
 const numeroTitulo = document.getElementById('numero-titulo');
 const texto = document.getElementById('texto');
 
 numeroTitulo.innerHTML = numero;
-texto.innerHTML = '<p>Seu número +</p>'
+texto.innerHTML = `<p> Raiz quadrada: ${numero ** 0.5} </p>`;
+texto.innerHTML = `<p> ${numero ** 0.5}  é inteiro: </p>`;
+texto.innerHTML = `<p> Raiz quadrada: ${numero ** 0.5} </p>`;
+texto.innerHTML = `<p> Raiz quadrada: ${numero ** 0.5} </p>`;
+texto.innerHTML = `<p> Raiz quadrada: ${numero ** 0.5} </p>`;
+texto.innerHTML = `<p> Raiz quadrada: ${numero ** 0.5} </p>`;
+texto.innerHTML = `<p> Raiz quadrada: ${numero ** 0.5} </p>`;
