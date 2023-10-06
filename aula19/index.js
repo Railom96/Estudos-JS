d552ec8e29f2f17d349b581bae5ef673bae2b095
Israel Moreira <86491370+Railom96@users.noreply.github.com>
2023-10-05 22:22:00 -0300

    aula20
    exercício aula 20

---
+++ aula19/index.js
@@ -0,0 +1,10 @@
+/*
+Dados primitivos (imutaveis | Dado = Valor) - String, number, boolean, undefined, null, (bigint, symbol) - Valores copiados
+
+
+Referencia (Mutável) - Array, object, function - Valor por referencia
+
+Arrays são colchetes e objetos são chaves
+*/
+
+
---
+++ aula20/index.js
@@ -0,0 +1,44 @@
+function meuEscopo () {
+    const form = document.querySelector('.form')
+    const resultado = document.querySelector('.resultado')
+
+    const pessoas = [];
+    
+    function recebeEventoForm (evento) {
+        const nome = form.querySelector('.nome');
+        const sobrenome = form.querySelector('.sobrenome');
+        const peso = form.querySelector('.peso');
+        const altura = form.querySelector('.altura');
+        evento.preventDefault();
+
+        console.log(nome.value, sobrenome.value, peso.value, altura.value);
+
+        pessoas.push ({
+            nome: nome.value,
+            sobrenome: sobrenome.value,
+            peso: peso.value,
+            altura: altura.value
+        });
+
+        console.log(pessoas);
+
+        resultado.innerHTML += ` <p> ${nome.value} ${sobrenome.value} ${peso.value} ${altura.value}  </p> `
+    }
+
+    form.addEventListener('submit', recebeEventoForm);
+}
+meuEscopo ();
+
+
+    //form.onsubmit = function (evento) {
+    //    evento.preventDefault();
+    //   alert('socorro!');
+    //    console.log('Foi enviado.');
+    //};
+
+    //let contador = 1;
+    //function recebeEventoForm (evento) {
+    //    evento.preventDefault();
+    //    console.log(`Form não foi enviado ${contador}`);
+    //    contador++;
+    //};
