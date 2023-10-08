f719a3a327daaae6c304ff8371e452e93774f4ad
Israel Moreira <86491370+Railom96@users.noreply.github.com>
2023-10-08 13:35:47 -0300

    aula23
    aula23

---
+++ aula23/index.js
@@ -0,0 +1,14 @@
+/**
+&& -> false && true -> false - Todas as expressoões precisa ser verdadeiras para que "AND" retorne verdadeiro
+|| -> true || false ->  - Todas as expressoões precisa ser falsas para que "OR" retorne falso
+
+-------------------------------
+
+Valores avalidos em false no JS:
+strings vazias - '' "" ``
+null / indefined
+NaN
+Qualquer coisa diferente disso, no JS é avaliado como true. 
+
+------------------
+ */
