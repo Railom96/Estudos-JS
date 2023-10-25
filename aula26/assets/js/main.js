d705cfd6fd82156930a69605e6f39582846e6f21
Israel Moreira <86491370+Railom96@users.noreply.github.com>
2023-10-25 11:39:55 -0300

    att

--- aula26/assets/js/main.js
+++ aula26/assets/js/main.js
@@ -2,4 +2,16 @@ let peso
 let altura 
 let resultado
 
-resultado = 
+
+resultado = peso / (altura * altura)
+
+function recebeForm () {
+    document.querySelector('.inputPeso');
+    document.querySelector('.inputAltura');
+    evento.preventDefault();
+}
+
+
+document.getElementById('mostraImc')
+
+mostraImc.iner
