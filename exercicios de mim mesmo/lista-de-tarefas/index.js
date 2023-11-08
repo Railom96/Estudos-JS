3ee5e7c10eff57789a92e10341b0d4b9e7838031
Israel Moreira <86491370+Railom96@users.noreply.github.com>
2023-11-08 10:15:05 -0300

    att

--- exercicios de mim mesmo/lista-de-tarefas/index.js
+++ exercicios de mim mesmo/lista-de-tarefas/index.js
@@ -1,1 +1,30 @@
-console.log ("oi mundo!")
+function criaTarefaAFazer(){
+    const tarefaAfazer = (document.getElementById(inputAFazer).value);
+    const listaAFazer = [];
+    listaAFazer.push({ nome: tarefaAfazer });
+}
+
+function criaTarefaEmAndamento(){
+    const tarefaEmAndamento = (document.getElementById(inputEmAndamento).value);
+    const listaEmAndamento = [];
+    listaEmAndamento.push({ nome: tarefaEmAndamento });
+}
+
+function criaTarefaFinalizada(){
+    const tarefaFinalizada = (document.getElementById(inputFinalizado).value);
+    const listaFinalizada = [];
+    listaFinalizada.push({ nome: tarefaFinalizada });
+}
+
+
+
+
+function recebeEventoForm(evento) {
+    evento.preventDefault();
+    criaTarefaAFazer();
+    criaTarefaEmAndamento();
+    criaTarefaFinalizada();
+}
+
+const form = document.getElementById('form-aFazer', 'form-emAndamento', 'form-Finalizado');
+form.addEventListener('submit', recebeEventoForm);
